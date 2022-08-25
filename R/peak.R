6ee92675c6e3e75a0628fae29e0b1b86001c9bc3
Rico Derks <r.j.e.derks@lumc.nl>
2022-08-25 09:28:35 +0200

    This creates a Convergence failure: singular convergence (7) error.

--- R/peak.R
+++ R/peak.R
@@ -1,6 +1,6 @@
-#' @title Create peak
+#' @title Peak fitting function
 #'
-#' @description Create a peak with shape ....
+#' @description Peak fitting function
 #'
 #' @param v WIP
 #' @param v0 WIP
@@ -17,7 +17,7 @@
 #'
 peak <- function(v, v0, fwhh, p, a, b) {
   #r <- 1 / (1 + exp(-p)) logistic function reaches limits too fast
-  r <- atan(p) * 2 / pi  # arctan has same basic shape but reaches limits much slower
+  r <- 0.5 + (atan(p) / pi)  # arctan has same basic shape but reaches limits much slower
   ga <- exp(log(2) * (1 - (2 * (v - v0) / fwhh)^2)) * sqrt(log(2) / pi) * (1 / fwhh)
   lo <- (1 / (fwhh^2 + 4 * (v - v0)^2)) * (2 * fwhh / pi)
 
