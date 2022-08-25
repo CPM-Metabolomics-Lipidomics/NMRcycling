559b03a045e886c647f9fcf000b633ec05e26d31
Rico Derks <r.j.e.derks@lumc.nl>
2022-08-25 08:10:40 +0200

    Added peakfitting functions.

---
+++ R/peak.R
@@ -0,0 +1,25 @@
+#' @title Create peak
+#'
+#' @description Create a peak with shape ....
+#'
+#' @param v WIP
+#' @param v0 WIP
+#' @param fwhh WIP
+#' @param p WIP
+#' @param a WIP
+#' @param b WIP
+#'
+#' @return  WIP
+#'
+#' @export
+#'
+#' @author Aswin Verhoeven
+#'
+peak <- function(v, v0, fwhh, p, a, b) {
+  #r <- 1 / (1 + exp(-p)) logistic function reaches limits too fast
+  r <- atan(p) * 2 / pi  # arctan has same basic shape but reaches limits much slower
+  ga <- exp(log(2) * (1 - (2 * (v - v0) / fwhh)^2)) * sqrt(log(2) / pi) * (1 / fwhh)
+  lo <- (1 / (fwhh^2 + 4 * (v - v0)^2)) * (2 * fwhh / pi)
+
+  return(b + a * (r * ga + (1 - r) * lo))
+}
---
+++ R/peakfit.R
@@ -0,0 +1,86 @@
+#' @title Do a peak fit
+#'
+#' @description Do peak fit
+#'
+#' @param v WIP
+#' @param v0start WIP
+#' @param fwhhstart WIP
+#' @param pstart WIP
+#' @param astart WIP
+#' @param bstart WIP
+#' @param sp WIP
+#'
+#' @return  WIP
+#'
+#' @export
+#'
+#' @importFrom stats nls nls.control
+#'
+#' @author Aswin Verhoeven
+#'
+peakfit <- function(v, v0start, fwhhstart, pstart, astart, bstart, sp) {
+  dffit <- data.frame(v, sp)
+
+  fit <- stats::nls(
+    sp ~ peak(v, v0start, fwhhstart, pstart, a, bstart),
+    data = dffit,
+    start = list(a = astart),
+    algorithm = "port",
+    control = stats::nls.control(maxiter = 500)
+  )
+  afit <- summary(fit)[[10]][, "Estimate"]
+
+  fit <- stats::nls(
+    sp ~ peak(v, v0start, fwhhstart, pstart, a, b),
+    data = dffit,
+    start = list(a = afit, b = bstart),
+    algorithm = "port",
+    control = stats::nls.control(maxiter = 500)
+  )
+  afit <- summary(fit)[[10]]["a", "Estimate"]
+  bfit <- summary(fit)[[10]]["b", "Estimate"]
+
+  fit <- stats::nls(
+    sp ~ peak(v, v0, fwhhstart, pstart, a, b),
+    data = dffit,
+    start = list(a = afit, b = bfit, v0 = v0start),
+    algorithm = "port",
+    control = stats::nls.control(maxiter = 500)
+  )
+  afit <- summary(fit)[[10]]["a", "Estimate"]
+  bfit <- summary(fit)[[10]]["b", "Estimate"]
+  v0fit <- summary(fit)[[10]]["v0", "Estimate"]
+
+  fit <- stats::nls(
+    sp ~ peak(v, v0, fwhh, pstart, a, b),
+    data = dffit,
+    start = list(a = afit,
+                 b = bfit,
+                 v0 = v0fit,
+                 fwhh = fwhhstart),
+    algorithm = "port",
+    control = stats::nls.control(maxiter = 500)
+  )
+  afit <- summary(fit)[[10]]["a", "Estimate"]
+  bfit <- summary(fit)[[10]]["b", "Estimate"]
+  v0fit <- summary(fit)[[10]]["v0", "Estimate"]
+  fwhhfit  <- summary(fit)[[10]]["fwhh", "Estimate"]
+
+  fit <- stats::nls(
+    sp ~ peak(v, v0, fwhh, p, a, b),
+    data = dffit,
+    start = list(a = afit,
+                 b = bfit,
+                 v0 = v0fit,
+                 fwhh = fwhhfit,
+                 p = pstart),
+    algorithm = "port",
+    control = stats::nls.control(maxiter = 500)
+  )
+
+  result <- as.list(summary(fit)[[10]][, "Estimate"])
+  result$a <- ifelse(result$a < 0, 0, result$a)
+  result$v <- v
+
+  return(result)
+}
