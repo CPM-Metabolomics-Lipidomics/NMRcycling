b4a8e9b9a7d6901d832e354850b574640560fc2c
Aswin <99652441+asve73@users.noreply.github.com>
2024-02-15 14:31:46 +0100

    Update peakfitsimple.R: added function/parameter descriptions

--- R/peakfitsimple.R
+++ R/peakfitsimple.R
@@ -1,3 +1,30 @@
+#' @title Perform a peak fit
+#'
+#' @description Function that fits a mixed Gaussian-Lorentzian function to a supplied spectral region. It does this in
+#' several steps. These are:
+#' 1. Optimize peak area.
+#' 2. Optimize the peak area and the baseline level.
+#' 3. Same as 2., but also optimize the peak position.
+#' The difference between the function *peakfitsimple* and the function *peakfit* is that *peakfit* also optimizes the peak width and
+#' the peak shape, while *peakfitsimple* does not. See also the function *peak*.
+#'
+#' @param v Numeric vector containing the axis values (frequencies, chemical shifts...) for the points in the spectral region.
+#' @param v0start Starting value for the peak position optimization.
+#' @param fwhhstart Value for the peak width (full width at half height).
+#' @param pstart Value for the peak shape (Gaussian / Lorentzian).
+#' @param astart Starting value for the peak area optimization.
+#' @param bstart Starting value for the baseline level optimization.
+#' @param sp Numeric vector containing a spectral region, ideally containing a single peak.
+#'
+#' @return  A list with the optimized peak parameters.
+#'
+#' @export
+#'
+#' @importFrom stats nls nls.control
+#'
+#' @author Aswin Verhoeven
+#'
+
 #' @title Do a simple peak fit
 #'
 #' @description Do simple peak fit
